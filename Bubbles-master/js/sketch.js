40b1015bbad4a8111f876c4d4732c6023acba426
NCR117 <116882888+NCR117@users.noreply.github.com>
2022-12-02 12:17:38 +0000

    Add files via upload

---
+++ Bubbles-master/js/sketch.js
@@ -0,0 +1,32 @@
+
+
+
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");
+    background(220);
+    colorMode(HSB);
+
+    for(var i = 0; i < 150; i++){
+
+circles();
+    }
+
+function draw() {
+
+}
+
+function circles() {
+
+    var x = random(width); // establishing new variables
+    var y = random(height);
+    var d = random(5, 150);
+    var hue = random(120, 225) //new variable, colour hue, randomly chosen between 120, 225 (blues)
+
+noStroke();
+
+    fill(hue, 255, 255, 0.1);
+    circle(x, y, d);
+}
+
+}
